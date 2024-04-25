59a8149c6d745771e1e43450be8cbf81af217554
Simon Berlendis <simon.berlendis@sogilis.com>
2024-04-25 12:35:25 +0200

    WIP

--- Python/method.py
+++ Python/method.py
@@ -3,7 +3,7 @@ class AuthenticationService:
         return id == 12345
 
     def is_authenticated_new(self, role, id):
-        return id == 12345
+        return is_authenticated(id)
 
     '''
     the goal is to replace the method above with this one:
