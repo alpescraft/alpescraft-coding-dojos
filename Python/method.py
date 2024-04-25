5f6eba28778a8013ad68ebfa4a20607fcf57bf75
Simon Berlendis <simon.berlendis@sogilis.com>
2024-04-25 12:34:21 +0200

    WIP

--- Python/method.py
+++ Python/method.py
@@ -2,7 +2,7 @@ class AuthenticationService:
     def is_authenticated(self, id):
         return id == 12345
 
-    def is_authenticated(self, id):
+    def is_authenticated_new(self, role, id):
         return id == 12345
 
     '''
