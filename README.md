a544cf775e71be7df31ec5cd1fa97b738bdac6c8
Xavier Nopre <xnopre@gmail.com>
2025-07-01 13:11:47 +0200

    Session du 01/06/2025 : Kata du calcul de prix avec API Fluent

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Séance du 17/04/2025
+# Séance du 01/06/2025
 
-# Kata : Calcul de prix
+# Calcul de prix avec API fluent
 
 Implémenter le calcul du prix total sous forme de chaine de caractères à partir des paramètres suivants :
 
@@ -21,3 +21,26 @@ Prendre en compte des réductions :
 - Réductions :
   - 1000 € → Remise 3% : 5 x 345,00 € + taxe 10% → “1840.58 €”
   - 5000 € → Remise 5% : 5 x 1299,00 € + taxe 10% → “6787.28 €”
+
+# Objectif 1 : Mettre en place une API fluent
+
+Une "API fluent" est une architecture du code qui permet d'enchainer les appels de méthodes :
+
+```shell
+  un_objet_ou_une_classe
+    .withQuantity(3)
+    .withUnitPrice(1.21)
+    .withTax(5)
+    ...
+```
+
+# Objectif 2 : guider l'utilisateur
+
+Faire en sorte que le développeur utilisateur de ce code n'ait pas de doute et ne
+puisse pas se tromper lors de l'utilisation de cette API :
+
+- Ne pas pouvoir se tromper dans les typages des données
+- Ne pas pouvoir appeler n'importe quel enchainement de méthode
+- Ne pas pouvoir appeler plusieurs fois une méthode si cela n'a pas de sens
+- Pouvoir utiliser certaines méthodes de façon optionnelle (ex : tax)
+- ...
--- app.test.ts
+++ app.test.ts
@@ -1,56 +1,96 @@
-function getDiscountRate(price: number) {
-  switch (true) {
-    case price >= 5000:
-      return 5;
-    case price >= 1000:
-      return 3;
-    default:
-      return 0;
+class Article {
+  price(price: number) {
+    return new ArticleWithPrice(price);
+  }
+
+  quantity(quantity: number) {
+    return new ArticleWithQuantity(quantity);
   }
 }
 
-function applyDiscount(price: number) {
-  const discountRate = getDiscountRate(price);
-  return price * (1 - discountRate / 100);
+class ArticleWithPrice {
+  #price: number;
+
+  constructor(price: number) {
+    this.#price = price;
+  }
+
+  quantity(quantity: number) {
+    return new ArticleWithPriceAndQuantity(this.#price, quantity);
+  }
 }
 
-function computePrice(
-  quantity: number,
-  unitPrice: number,
-  tax: number,
-): string {
-  const price = applyDiscount(quantity * unitPrice * (1 + tax / 100));
-  return `${price.toFixed(2)} €`;
+class ArticleWithQuantity {
+  #quantity = 0;
+
+  constructor(quantity: number) {
+    this.#quantity = quantity;
+  }
+
+  price(price: number) {
+    return new ArticleWithPriceAndQuantity(price, this.#quantity);
+  }
 }
 
-describe("calcul des prix", () => {
-  it("should handle (3 items) tax free", () => {
-    // * HT : 3 articles à 1,21 € + 0 % ⇒ “3.63 €”
-    const price = computePrice(3, 1.21, 0);
-    expect(price).toEqual("3.63 €");
+class ArticleWithPriceAndQuantity {
+  #price: number;
+  #quantity: number;
+
+  constructor(price: number, quantity: number) {
+    this.#price = price;
+    this.#quantity = quantity;
+  }
+
+  computedPrice() {
+    return new ArticleWithPriceAndQuantityAndTax(
+      this.#price,
+      this.#quantity,
+      0,
+    ).computedPrice();
+  }
+
+  tax(tax: number) {
+    return new ArticleWithPriceAndQuantityAndTax(
+      this.#price,
+      this.#quantity,
+      tax,
+    );
+  }
+}
+
+class ArticleWithPriceAndQuantityAndTax {
+  #price: number;
+  #quantity: number;
+  #tax = 0;
+
+  constructor(price: number, quantity: number, tax: number) {
+    this.#price = price;
+    this.#quantity = quantity;
+    this.#tax = tax;
+  }
+
+  computedPrice() {
+    const totalPrice = this.#price * this.#quantity * (1 + this.#tax / 100);
+    return `${totalPrice.toFixed(2)} €`;
+  }
+}
+
+describe("kata", () => {
+  it("should calculate simple price", () => {
+    const article = new Article();
+    const price = article.price(1.21).quantity(3).computedPrice();
+    expect(price).toBe("3.63 €");
   });
-  it("should handle (6 items) tax free", () => {
-    const price = computePrice(6, 1.21, 0);
-    expect(price).toEqual("7.26 €");
+
+  it("should calculate other simple price", () => {
+    const article = new Article();
+    const price = article.quantity(5).price(3.42).computedPrice();
+    expect(price).toBe("17.10 €");
   });
-  it("should handle a 5% tax with rounding down to 2 digits", () => {
-    // TTC 5% : 3 articles à 1,21 € + 5 % ⇒ “3.81 €”
-    const price = computePrice(3, 1.21, 5);
-    expect(price).toEqual("3.81 €");
-  });
-  it("should handle a 20% tax with rounding up", () => {
-    // TTC 20%: 3 articles à 1,21 € +20 % ⇒ “4.36 €”
-    const price = computePrice(3, 1.21, 20);
-    expect(price).toEqual("4.36 €");
-  });
-  it("should handle a 3% discount when total >= 1000", () => {
-    // 1000 € → Remise 3% : 5 x 345,00 € + taxe 10% → “1840.58 €”
-    const price = computePrice(5, 345, 10);
-    expect(price).toEqual("1840.58 €");
-  });
-  it("should handle a 5% discount when total >= 5000", () => {
-    // 5000 € → Remise 5% : 5 x 1299,00 € + taxe 10% → “6787.28 €”
-    const price = computePrice(5, 1299, 10);
-    expect(price).toEqual("6787.28 €");
+
+  it("should calculate price with tax", () => {
+    const article = new Article();
+    const price = article.quantity(3).price(1.21).tax(5).computedPrice();
+    expect(price).toBe("3.81 €");
   });
 });
