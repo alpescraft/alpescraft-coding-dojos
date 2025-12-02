b7f56d4af0f03d1f8316d5a146af21e1ebe2eda1
Xavier Nopre <xnopre@gmail.com>
2025-12-02 15:53:17 +0100

    Session du 02/12/2025 : Suite de Conway

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
-> Séance du 30/10/2025
+> Séance du 02/12/2025
 
-# Roman numerals
+# Suite de Conway
 
-Convertir dees nombres arabes en nombre romains :
+## Objectif du kata
 
-```text
-   1 --> I
-   2 --> II
-   4 --> IV
-   7 --> VII
-  10 --> X
-  50 --> L
- 100 --> C
- 500 --> D
-1000 --> M
+Développer l'algorithme qui permte de passer d'un terme de la suite de Conway au suivant.
+
+## Principe de la suite de Conway
+
+Le premier terme de la suite de Conway est posé comme égal à "1". Chaque terme de la suite se construit en décrivant
+le terme précédent, c'est-à-dire en énonçant le nombre de fois où chaque chiffre est répété,
+suivi du chiffre en question.
+
+Concrètement :
+
+`X0 = 1`
+
+Ce terme comporte simplement un « 1 ». Par conséquent, le terme suivant est :
+
+`X1 = 11`
+
+Celui-ci est composé de deux « 1 » :
+
+`X2 = 21`
+
+En poursuivant le procédé :
+
+```
+X3 = 12 11
+X4 = 11 12 21
+X5 = 31 22 11
+X6 = 13 11 22 21
 ```
 
-## Variante
+# Idées évoquées pour ce kata
 
-Convertir dans l'autre sens.
-
-# Résumé de la séance
-
-Nous nous sommes arrêtés après avoir mis de côté le cas du "4" pour mettre en place
-une `Map` avec les seuils et leurs symboles, et un algorithme générique permettant
-de les exploiter et les cumuler.
-
-Reste à voir le cas du "4" et plus généralement les cas où il faut précédent un symbole
-par un autre pour faire une soustraction.
+- Utiliser la récursivité
--- app.test.ts
+++ app.test.ts
@@ -1,54 +1,34 @@
-const map = new Map();
-// map.set(4, "IV");
-map.set(100, "C");
-map.set(50, "L");
-map.set(10, "X");
-map.set(5, "V");
-map.set(1, "I");
-
-function convert(number: number): string {
-  for (const key of map.keys()) {
-    if (number >= key) {
-      return map.get(key) + convert(number - key);
+function conway(input: string) {
+  let currentValue = input.at(0)!;
+  let result = "";
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === currentValue) {
+      count++;
+      continue;
     }
+    result += count + currentValue;
+    currentValue = input.at(i)!;
+    count = 1;
   }
-  return "";
+  result += count + currentValue;
+  return result;
 }
 
-describe("int to romain converter", () => {
-  it("Should convert plain symbols", () => {
-    expect(convert(1)).toEqual("I");
-    expect(convert(5)).toEqual("V");
-    expect(convert(10)).toEqual("X");
-    expect(convert(50)).toEqual("L");
-    expect(convert(100)).toEqual("C");
+describe("Conway", () => {
+  it("test 1", () => {
+    expect(conway("1")).toEqual("11");
   });
-  it("Should convert 2 to II", () => {
-    expect(convert(2)).toEqual("II");
+  it("test 11", () => {
+    expect(conway("11")).toEqual("21");
   });
-  it("Should convert 3 to III", () => {
-    expect(convert(3)).toEqual("III");
+  it("test 21", () => {
+    expect(conway("21")).toEqual("1211");
   });
-  it("Should convert 6 to VI", () => {
-    expect(convert(6)).toEqual("VI");
+  it("test 1211", () => {
+    expect(conway("1211")).toEqual("111221");
   });
-  it("Should convert 8 to VIII", () => {
-    expect(convert(8)).toEqual("VIII");
-  });
-  it("Should convert 11 to XI", () => {
-    expect(convert(11)).toEqual("XI");
-  });
-  it("Should convert 30 to XXX", () => {
-    expect(convert(30)).toEqual("XXX");
-  });
-  it("Should convert 37 to XXXVII", () => {
-    expect(convert(37)).toEqual("XXXVII");
-  });
-  // TODO
-  it.skip("Should convert 4 to IV", () => {
-    expect(convert(4)).toEqual("IV");
-  });
-  it.skip("Should convert 9 to IX", () => {
-    expect(convert(9)).toEqual("IX");
-  });
+
+  // Cas d'erreur:
+  // test tequilla
 });
